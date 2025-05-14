5c89cc07fbe7248bef370e3a4c6e7a62e9b3e1bb
Jeyaresh <jeyareshdj5@gmail.com>
2025-05-14 12:32:01 +0530

    card slider added

--- assets/js/script.js
+++ assets/js/script.js
@@ -162,3 +162,36 @@ window.addEventListener("mousemove", function (event) {
 
 
 
+
+new Swiper('.card-wrapper', {
+ 
+    loop: true,
+    spaceBetween: 20,
+    
+  
+    // pagination bullets
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+breakpoints: {
+    0:{
+        slidesPerView: 1,
+    },
+    768:{
+        slidesPerView: 2,
+    },
+    1024:{
+        slidesPerView: 3,
+    },
+}
+
+  });
